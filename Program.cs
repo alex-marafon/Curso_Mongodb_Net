f35a23131bb04dd7124678781394ce24c817a398
Alex Marafon <marafon@mbrtech.com.br>
2021-11-04 19:54:51 -0400

    Projeto Base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso_Mongodb_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
